8fe7a52dbd8455b66b23a926c8233523670bc41e
Alex <olivesargreek@gmail.com>
2023-06-16 15:52:02 +0300

    feat: currency change by JS magic

---
+++ scripts/script.js
@@ -0,0 +1,72 @@
+document.getElementById('main-action-button').onclick = function() {
+  document.getElementById('products').scrollIntoView({ behavior: 'smooth'});
+};
+
+let links = document.querySelectorAll('.menu-item > a');
+
+for (let i = 0; i < links.length; i++) {
+  links[i].onclick = function () {
+    document.getElementById(links[i].getAttribute('data-link')).scrollIntoView({ behavior: 'smooth'});
+  };
+}
+
+let buttons = document.getElementsByClassName('product-button');
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].onclick = function () {
+    document.getElementById('order').scrollIntoView({ behavior: 'smooth'});
+  };
+}
+
+let burger =  document.getElementById('burger');
+let name =  document.getElementById('name');
+let phone =  document.getElementById('phone');
+
+document.getElementById('order-action').onclick = function () {
+  let hasError = false;
+
+  [burger, name, phone].forEach(item => {
+    if (!item.value) {
+      item.parentElement.style.background = 'red';
+      hasError = true;
+    } else {
+      item.parentElement.style.background = '';
+    }
+  });
+
+  if (!hasError) {
+    [burger, name, phone].forEach(item => {
+      item.value = '';
+    });
+    alert("Спасибо за заказ! Мы скоро свяжемся с вами!");
+  }
+};
+
+let prices = document.getElementsByClassName('products-items-price');
+
+document.getElementById('change-currency').onclick = function(event) {
+  let currentCurrency = event.target.innerText;
+
+  let newCurrency = '$';
+  let cef = 1; 
+
+  if (currentCurrency === '$') {
+    newCurrency = '₽';
+    cef = 89;
+  } else if (currentCurrency === '₽') {
+    newCurrency = 'BYN';
+    cef = 3;
+  } else if (currentCurrency === 'BYN') {
+    newCurrency = '€';
+    cef = 0.9;
+  } else if (currentCurrency === '€') {
+    newCurrency = '¥';
+    cef = 6.9;
+}
+
+  event.target.innerText = newCurrency;
+
+  for (let i = 0; i < prices.length; i++) {
+    prices[i].innerText = +(prices[i].getAttribute('data-base-price') * cef).toFixed(1) + " " + newCurrency;
+  } 
+};
